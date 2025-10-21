cf377caf4ad43e8071dde66d4e109481ffa05f8f
tanbopp <sltnsvenn2@gmail.com>
2025-10-21 18:50:28 +0700

    refactor: remove outdated SEO example and old service index files, enhance support page layout and styles

---
+++ resources/views/template-blade/example.blade.php
@@ -0,0 +1,23 @@
+{{-- Layout --}}
+@extends('partials.layouts.main')
+
+{{-- Title --}}
+@section('title', '')
+
+{{-- Navbar --}}
+@section('navbar')
+@endsection
+
+{{-- SEO & Meta Tags --}}
+@section('seoMetaTags')
+@endsection
+
+{{-- Style CSS --}}
+@section('style-css')
+    <style>
+    </style>
+@endsection
+
+{{-- Content --}}
+@section('content')
+@endsection
